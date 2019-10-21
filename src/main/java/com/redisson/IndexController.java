5745033dd3ea07e3f2c76608d45042cd5dd41a66
pengzhengfa <pengzhengfa@alibaba.com>
2019-10-21 14:58:37 +0800

    工程优化

--- src/main/java/com/redisson/IndexController.java
+++ src/main/java/com/redisson/IndexController.java
@@ -5,6 +5,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import java.util.UUID;
+import java.util.concurrent.TimeUnit;
 
 /**
  * 利用java锁来模拟扣库存
@@ -22,14 +24,20 @@ public class IndexController {
 
     /**
      * 我们来这样子写分布式锁有什么问题呢,万一我执行到某一行代码抛异常了呢
+     * 也有可能出现当我执行到某一段代码之间,我们的服务器发生宕机呢,那么我的key
+     * 会长久的存在于redis当中,也有可能我这个线程的key,被另外一个线程删除(锁失效)
+     * 那么这个问题该怎么解决呢
      *
      * @return
      */
     @RequestMapping("/deduct_stock")
     public String deductStock() {
+        String lockKey = "product_001";
+        String clientId = UUID.randomUUID().toString();
         try {
-            String lockKey = "lockKey";
-            Boolean result = stringRedisTemplate.opsForValue().setIfAbsent("lockKey", "zhuge");
+            Boolean result = stringRedisTemplate.opsForValue().setIfAbsent(lockKey, clientId,10,TimeUnit.SECONDS);
+            //设置超时时间
+            stringRedisTemplate.expire(lockKey,30,TimeUnit.SECONDS);
             if (!result) {
                 return "1001";
             }
@@ -43,7 +51,10 @@ public class IndexController {
             }
 
         }finally {
-            stringRedisTemplate.delete("lockKey");
+            //谁加的锁,谁去释放
+            if (clientId.equals(stringRedisTemplate.opsForValue().get(lockKey))){
+                stringRedisTemplate.delete("lockKey");
+            }
         }
         return "end";
     }
